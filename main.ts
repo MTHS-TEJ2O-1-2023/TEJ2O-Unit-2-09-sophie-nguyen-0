5d18bf634edd97db0ff588d3797ee2a3fad9efb7
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-10 13:18:21 -0400

    readme

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: sophie
+ * Created on: Oct 2023
+ * This program does rock paper scissors
 */
 
 basic.showString('Hello, World!')
