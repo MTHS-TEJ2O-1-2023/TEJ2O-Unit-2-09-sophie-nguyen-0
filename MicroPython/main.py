b1d2d579a37cad047b85178c7cde4e5dba89d571
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-11 17:26:43 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,20 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: sophie
+Created on: Oct 2023
+This module is a Micro:bit MicroPython program does rock paper scissors
 """
 
 from microbit import *
 
+display.clear
+display.show(Image.Heart)
 
-display.scroll("Hello, World!")
+random_number = 0
+score = 0
+
+while True:
+    if button_a.is_pressed():
+        score = score + 1
+
+    if button_b.is_pressed() :
+        display.scroll(str(score))
